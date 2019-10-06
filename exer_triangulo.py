dc2ba4547b660fa723576684a4858456ca33668f
raislansilva <raislan@live.com>
2019-10-06 17:12:03 -0300

    segundo commit

--- exer_triangulo.py
+++ exer_triangulo.py
@@ -1,16 +1,19 @@
 
 def triangulo(lado1,lado2,lado3):
+    value = None
     if lado1 > 99 or lado1 < 0 or lado2 >99 or lado2 < 0 or lado3>99 or lado3 <0:
-        return 0
+        value = 0
     elif lado3 >= lado1+lado2  or lado2 >= lado1+lado3  or  lado1 >= lado2+lado3:
         if lado3 <= lado1-lado2  or lado2 <= lado1-lado3 or lado1 <= lado2-lado3:
-            return 4
+            value = 4
     elif lado1 == lado2 and lado2 == lado3:
-        return 1
+         value = 1
     elif lado1 == lado2 and lado2 != lado3 or lado1 == lado3 and lado1 != lado2 or lado2 == lado3 and lado1 != lado3:
-        return 2
+        value = 2
     elif lado1 != lado2 and lado2 != lado3 and lado1 != lado3:
-        return 3    
+        value = 3
+
+    return value        
     
         
 
